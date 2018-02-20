ad43db4779ffd087d10b9751f5c017629ea4f63b
lm1370 <lm1370@york.ac.uk>
2018-02-20 14:56:18 +0000

    Documentation for Shape.java

--- Notes/src/main/java/com/legba/notes/elements/Shape.java
+++ Notes/src/main/java/com/legba/notes/elements/Shape.java
@@ -5,22 +5,26 @@ public class Shape extends Element {
 	public Position position;
 	public Colors colors;
 	
+	//Constructor with parameters position, shape and type
 	public Shape(String type) {
 		super("shape");
 
-
+		//Optional for Element 'Shape'
 		this.position = new Position((Element)this);
 		this.colors = new Colors((Element)this);
 		
+		//Required by Element 'Shape'
 		this.setType(type);
 		
 	}
 	
+	//Returns type of shape
 	public String getType() {
 		return (String)this.getAttribute("type");
 
 	}
-
+	
+	//Sets type of shape
 	public void setType(String type) {
 		// Only allow valid types to be set
 		if(type != "ellipse" && type != "rectangle" && type != "line") {
@@ -29,11 +33,13 @@ public class Shape extends Element {
 		this.setAttribute("type", type);
 	}
 	
+	//Returns pixel width of border on shape
 	public int getStroke() {
 		return (int)this.getAttribute("stroke");
 
 	}
-
+	
+	//Sets pixel width of border on shape
 	public void setStroke(String stroke) {
 		this.setAttribute("stroke", stroke);
 	}
