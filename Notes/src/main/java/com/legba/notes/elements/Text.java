daef2c474aae3fe905bf8d5c12c110a2128d7145
ruth.herd <ruth@downe.net>
2018-02-22 16:26:29 +0000

    Edited text element comments

--- Notes/src/main/java/com/legba/notes/elements/Text.java
+++ Notes/src/main/java/com/legba/notes/elements/Text.java
@@ -1,7 +1,7 @@
 package com.legba.notes.elements;
 
 import com.legba.notes.elements.Element;
-
+//Rich text element
 public class Text extends Element {
 
 	public Formats format;
@@ -9,9 +9,11 @@ public class Text extends Element {
 	public Transition transition;
 	public Position position;
 	
+	//Constructor with parameters format, color, transition and position
 	public Text() {
 		super("text");
 		
+		//Optional for text element
 		this.format = new Formats((Element)this);
 		this.colors = new Colors((Element)this);
 		this.transition = new Transition((Element)this);
