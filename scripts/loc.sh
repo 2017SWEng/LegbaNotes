4c2368249503043a568a67a6273e620b5934c050
Zak West <zraw500@york.ac.uk>
2018-02-28 01:21:24 +0000

    Change data.csv

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -11,4 +11,4 @@ echo "" >> $REPORT
 echo "" >> $REPORT
 echo "Lines of code by file" >> $REPORT
 ./node_modules/sloc/bin/sloc --details --format cli-table --keys total,source,comment,empty ../Notes/src/ >> $REPORT
-./node_modules/sloc/bin/sloc --details --format csv --keys total,source,comment,empty ../Notes/src/ > $DATA
+./node_modules/sloc/bin/sloc --format csv --keys total,source,comment,empty ../Notes/src/ > $DATA
