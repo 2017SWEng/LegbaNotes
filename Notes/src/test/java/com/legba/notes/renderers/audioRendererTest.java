0949262d1afb4fe7e751d59e98baa65f606dc8c1
Zak West <zraw500@york.ac.uk>
2018-03-01 21:16:48 +0000

    Fix tests

--- Notes/src/test/java/com/legba/notes/renderers/audioRendererTest.java
+++ Notes/src/test/java/com/legba/notes/renderers/audioRendererTest.java
@@ -2,28 +2,62 @@ package com.legba.notes.renderers;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.concurrent.CountDownLatch;
+
+import javax.swing.SwingUtilities;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
 
 import com.legba.notes.elements.Audio;
 import com.legba.notes.renderers.AudioRenderer;
 
 public class audioRendererTest {
 
+	@Before
+	public void setup() throws InterruptedException {
+		// JavaFX is stupid, cannot create a Text item without a scene
+		// Here were using a Swing embed JFX panel to create one
+		// This is totally a hack, but it works so...
+		final CountDownLatch latch = new CountDownLatch(1);
+		SwingUtilities.invokeLater(new Runnable() {
+		    public void run() {
+		        new JFXPanel(); // initializes JavaFX environment
+		        latch.countDown();
+		    }
+		});
+		latch.await();
+}
+	
 	@Test
-	public void test() {
+	public void test(){
 		
 		
-		Audio audio = new Audio ("testData/audioTest.wav");
 		AudioRenderer audiorend = new AudioRenderer(); 
+		Audio audio = new Audio ("testData/audioTest.wav");
 		
-		Node n = audiorend.render(audio);
+
+	
+		VBox n = (VBox) audiorend.render(audio);
 		
 		assertNotNull(n);
+		
+		// label,button,mediaview
+		assertEquals(3,n.getChildren().size());
+		
+					
 	}
 
 }
