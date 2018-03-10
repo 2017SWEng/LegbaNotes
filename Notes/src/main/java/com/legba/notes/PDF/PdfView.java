867eda15e5a479ca2838806c84285c35a4056aa6
Harry <hjew501@york.ac.uk>
2018-03-10 16:02:35 +0000

    PDF Renderer easy-ish

--- Notes/src/main/java/com/legba/notes/PDF/PdfView.java
+++ Notes/src/main/java/com/legba/notes/PDF/PdfView.java
@@ -1,30 +1,87 @@
 package com.legba.notes.PDF;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
 import com.legba.notes.app.AppController;
-import com.legba.notes.elements.Shape;
 
-import javafx.application.Application;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.stage.Stage;
+
 
 import javafx.concurrent.Worker.State;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 
 public class PdfView {
 	
-	public void render(String url, AppController appController) {
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public Node render(String url) {
 		
-		
+		BorderPane borderPane = new BorderPane();
 		
 		WebView pdfViewer = new WebView();
 		WebEngine webEngine = pdfViewer.getEngine();
 		webEngine.setJavaScriptEnabled(true);
+		
+		//Navigation buttons for moving around PDF document
+		//When each button is clicked it carries out a function in pdf.html
+		Button pageUp = new Button("Next Page");
+		pageUp.setOnAction(new EventHandler<ActionEvent>() {
+			
+			
+
+			@Override
+			public void handle(ActionEvent e) {
+	        	webEngine.executeScript("nextpage()");
+			 }
+		});
+		
+		Button pageDown = new Button("Previous Page");
+		pageDown.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+	        	webEngine.executeScript("previouspage()");
+			 }
+		});
+		
+		Button first = new Button("<<");
+		first.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+	        	webEngine.executeScript("firstpage()");
+			 }
+		});
+		
+		Button last = new Button(">>");
+		last.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+	        	webEngine.executeScript("lastpage()");
+			 }
+		});
+		
+		//HBox holds all the buttons at top of pane
+		
+		HBox hbox = new HBox();
+		hbox.setPadding(new Insets(15, 12, 15, 12));
+		hbox.setSpacing(10);
+		hbox.setStyle("-fx-background-color: #535360;");
+		
+		hbox.getChildren().addAll(first, pageDown, pageUp, last);
+		borderPane.setTop(hbox);
+		borderPane.setCenter(pdfViewer);
+		
+		//Loads PDF in pdf.html 
 		
 		System.out.println("loadpdf(\"" + url + "\")");
 		webEngine.getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> {
@@ -34,16 +91,7 @@ public class PdfView {
 	    });
 
 		webEngine.load(this.getClass().getResource("pdf.html").toString());
-
 		
-		//PDFJS.getDocument(url);
-		
-		appController.getReferenceRoot().getChildren().addAll(pdfViewer);
-		
-		
-		
-		
-	}
-
-
+		return borderPane;
+		}
 }
