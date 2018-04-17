4253a079003441673ec05b183ba79b22a0289dca
ruth.herd <ruth@downe.net>
2018-04-17 08:11:30 +0100

    Fixed PDF loading issue

---
+++ Notes/src/test/java/com/legba/notes/nodes/PdfViewTest.java
@@ -0,0 +1,16 @@
+package com.legba.notes.nodes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PdfViewTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+
+}
