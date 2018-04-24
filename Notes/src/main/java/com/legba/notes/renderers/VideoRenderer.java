a64557bf081fbbef6be4b33235773f7729b5ee19
ruth.herd <ruth@downe.net>
2018-04-24 18:29:40 +0100

    Video Renderer and Test Complete

--- Notes/src/main/java/com/legba/notes/renderers/VideoRenderer.java
+++ Notes/src/main/java/com/legba/notes/renderers/VideoRenderer.java
@@ -8,6 +8,11 @@ import javafx.scene.Node;
 public class VideoRenderer extends Renderer<Video>{
 
 	@Override
+	/**
+	 * Takes a video and renders it
+	 * @param video The video to be rendered
+	 * @return returns a Node with the rendered mediaplayer
+	 */
 	public Node render(Video video) {
 		MovieView movieView = new MovieView(video.getPath(), video.getX(), video.getY(), video.getWidth(), video.getHeight());
         return movieView;
---
+++ Notes/src/test/java/com/legba/notes/renderers/VideoRendererTest.java
@@ -0,0 +1,37 @@
+package com.legba.notes.renderers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.legba.notes.elements.Video;
+import com.legba.notes.nodes.MovieView;
+
+public class VideoRendererTest {
+
+	@Test
+	public void test() {
+		VideoRenderer renderer = new VideoRenderer(); 
+		
+		//Sets up test video
+		Video video = new Video("local_file.mp4");
+		video.setX(10f);
+		video.setX2(30f);
+		video.setY(15f);
+		video.setY2(40f);
+		
+		//Renders the test video
+		MovieView n = (MovieView) renderer.render(video);
+		
+		
+		assertNotNull(n);
+		
+		//Tests that the rendered video has the same values as set before rendering
+		assertTrue(n.getMediaPlayer().getMedia().getSource().toString().endsWith("local_file.mp4"));
+		assertEquals(n.getLayoutX(), 10f, 0.01f);
+		assertEquals(n.getLayoutY(), 15f, 0.01f);
+		assertEquals(n.getWidth(), 20f, 0.01f);
+		assertEquals(n.getHeight(), 25f, 0.01f);
+	}
+
+}
